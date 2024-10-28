5bb9cfdce754ea14b6f93426da670595b07b1783
Gabriela <gabriela.priaro@gmail.com>
2024-10-28 16:48:11 +0000

    Implementaçao para calcular tabuada do numero escolhido.

--- TabuadaEstruturaCondicional/Program.cs
+++ TabuadaEstruturaCondicional/Program.cs
@@ -1,10 +1,26 @@
-﻿namespace TabuadaEstruturaCondicional
+﻿//Utilizar estrutura condional For:
+//Problema "tabuada"
+//Ler um número inteiro N, daí mostrar na tela a tabuada de N para 1 a 10, conforme exemplo. 
+
+using System.Runtime.Versioning;
+
+namespace TabuadaEstruturaCondicional
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int tabuada, resultado;
+
+            Console.Write("Deseja a tabuada para qual valor? ");
+            tabuada = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= 10; i++)
+            {
+                resultado = tabuada * i;
+
+                Console.WriteLine($"{tabuada} x {i} = {resultado}");
+            }
         }
     }
 }
